720249e7dd4c3fe00215707f24daf46f6eb3ec91
VolodymyrYasinskyi <streamvova7@gmail.com>
2023-04-20 18:01:39 +0300

    summary

--- src/route/index.js
+++ src/route/index.js
@@ -23,6 +23,41 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    title: 'Resume',
+
+    name: {
+      firstname: 'Ivan',
+      lastname: 'Ivanov',
+    },
+
+    position: 'Junior Fullstack JS Developer',
+
+    salary: ' $600 в місяць',
+
+    summary: {
+      title:'Summary',
+      text: `Open-minded for new technologies, with 1 years of
+      experience in development. Whenever I start to
+      work on a new project I learn the domain and try
+      to understand the idea of the project. Good team
+      player, every colleague is a friend to me.`,
+    }
+
+    social: {
+      email: {
+        text: 'ivanov@mail.com',
+        href: 'mailto:ivanov@mail.com',
+      },
+      phone: {
+        text: '+380670000123',
+        href: 'tel:+380670000123',
+      },
+      linkedin: {
+        text: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/dmytro-test',
+      },
+    },
   })
 })
 
